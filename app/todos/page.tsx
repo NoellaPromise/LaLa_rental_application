8d0b7643405ad2024c5f9918019f536302034345
Noella claire d'Assise Dushakimana <dushakenoella@gmail.com>
2025-02-24 11:04:46 +0200

    initiate todos feature

---
+++ app/todos/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div className="text-center p-4 font-semibold text-[#DD7210] text-2xl">
+      <h1>Todos app</h1>
+    </div>
+  );
+};
+
+export default page;
