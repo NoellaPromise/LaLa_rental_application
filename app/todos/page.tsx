95d7c926c1522c95d898e65bdb4397cea6c7514c
Noella claire d'Assise Dushakimana <dushakenoella@gmail.com>
2025-02-24 11:39:25 +0200

    add styles to the todos

--- app/todos/page.tsx
+++ app/todos/page.tsx
@@ -1,9 +1,20 @@
 import React from "react";
+import { CirclePlus } from "lucide-react";
 
 const page = () => {
   return (
-    <div className="text-center p-4 font-semibold text-[#DD7210] text-2xl">
-      <h1>Todos app</h1>
+    <div className="container mx-auto px-6 py-8">
+      <h1 className="text-center font-semibold text-[#DD7210] text-2xl">
+        Todos app
+      </h1>
+      <div className="flex  justify-center gap-4 p-8 ">
+        <input
+          type="text"
+          placeholder="Add Todos..."
+          className="text-center  rounded-full text-gray-600"
+        />
+        <CirclePlus />
+      </div>
     </div>
   );
 };
