f18df3bc09fdb33fac4b4055b1a43d7785e8eb85
Noella claire d'Assise Dushakimana <dushakenoella@gmail.com>
2025-03-03 10:19:52 +0200

    Add the weather

---
+++ app/weather/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <h1 className="text-center font-bold p-4">Today&apos;s weather</h1>
+    </div>
+  );
+};
+
+export default page;
